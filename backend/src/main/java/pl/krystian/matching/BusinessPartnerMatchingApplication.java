331498f0f9e36b7ac3f690567c8c397c9ee1e239
Krystian Jachna <krysti4nape@gmail.com>
2026-07-07 22:35:02 +0200

    Initial Spring Boot backend setup

---
+++ backend/src/main/java/pl/krystian/matching/BusinessPartnerMatchingApplication.java
@@ -0,0 +1,13 @@
+package pl.krystian.matching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessPartnerMatchingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessPartnerMatchingApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/pl/krystian/matching/BusinessPartnerMatchingApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.krystian.matching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessPartnerMatchingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
