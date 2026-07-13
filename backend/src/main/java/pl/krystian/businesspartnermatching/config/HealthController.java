513b6ed097d52be104f6c8a938452f4171633f5e
Krystian Jachna <krysti4nape@gmail.com>
2026-07-13 22:04:30 +0200

    Rename package structure to reflect business partner matching context

--- backend/src/main/java/pl/krystian/businesspartnermatching/config/HealthController.java
+++ backend/src/main/java/pl/krystian/businesspartnermatching/config/HealthController.java
@@ -1,4 +1,4 @@
-package pl.krystian.matching.config;
+package pl.krystian.businesspartnermatching.config;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
