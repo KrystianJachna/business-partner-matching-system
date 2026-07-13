1dc2a0caae1aabb035ff558670962880bba84e5e
Krystian Jachna <krysti4nape@gmail.com>
2026-07-13 22:30:01 +0200

    Add IndustryRepository interface for CRUD operations on Industry entity

---
+++ backend/src/main/java/pl/krystian/businesspartnermatching/catalog/IndustryRepository.java
@@ -0,0 +1,6 @@
+package pl.krystian.businesspartnermatching.catalog;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IndustryRepository extends JpaRepository<Industry, Long> {
+}
