6eb3db2c4278ff4ea7c57d4ba7cee53f8684f509
Krystian Jachna <krysti4nape@gmail.com>
2026-07-13 23:37:26 +0200

    Use Lombok's @RequiredArgsConstructor in IndustryService for dependency injection

--- backend/src/main/java/pl/krystian/businesspartnermatching/catalog/industry/IndustryService.java
+++ backend/src/main/java/pl/krystian/businesspartnermatching/catalog/industry/IndustryService.java
@@ -1,17 +1,15 @@
 package pl.krystian.businesspartnermatching.catalog.industry;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class IndustryService {
 
     private final IndustryRepository industryRepository;
-
-    public IndustryService(IndustryRepository industryRepository) {
-        this.industryRepository = industryRepository;
-    }
 
     public List<Industry> getAllActiveIndustries() {
         return industryRepository.findAllByActiveTrueOrderByNameAsc();
