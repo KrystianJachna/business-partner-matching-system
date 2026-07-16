0c8b6864a7b9a983aa9aca33e21e9a4051f357c2
Krystian Jachna <krysti4nape@gmail.com>
2026-07-17 00:02:54 +0200

    Add PostgreSQL container support for integration tests, working mvn install

--- backend/src/test/java/pl/krystian/businesspartnermatching/BusinessPartnerMatchingApplicationTests.java
+++ backend/src/test/java/pl/krystian/businesspartnermatching/BusinessPartnerMatchingApplicationTests.java
@@ -2,12 +2,21 @@ package pl.krystian.businesspartnermatching;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.postgresql.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 @SpringBootTest
 class BusinessPartnerMatchingApplicationTests {
+
+	@Container
+	@ServiceConnection
+	static PostgreSQLContainer postgres =
+			new PostgreSQLContainer("postgres:16");
 
 	@Test
 	void contextLoads() {
 	}
-
 }
