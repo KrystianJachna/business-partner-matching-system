4f0461be2373c0b5fc0b087245cb124732a0d480
Krystian Jachna <krysti4nape@gmail.com>
2026-07-29 23:00:26 +0200

    Refactor matching algorithm implementation; replace PopularMatchingAlgorithm with a new MatchingAlgorithm interface for improved flexibility

--- backend/src/main/java/pl/krystian/businesspartnermatching/matching/algorithm/MatchingAlgorithm.java
+++ backend/src/main/java/pl/krystian/businesspartnermatching/matching/algorithm/MatchingAlgorithm.java
@@ -3,7 +3,7 @@ package pl.krystian.businesspartnermatching.matching.algorithm;
 import pl.krystian.businesspartnermatching.matching.algorithm.model.MatchingProblem;
 import pl.krystian.businesspartnermatching.matching.algorithm.model.PopularMatchingResult;
 
-public interface PopularMatchingAlgorithm<L, R> {
+public interface MatchingAlgorithm<L, R> {
 
     PopularMatchingResult<L, R> match(
             MatchingProblem<L, R> problem
